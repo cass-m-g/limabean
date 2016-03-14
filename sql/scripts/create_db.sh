edc7fff174b3785b522ad7ab580eb5c228c1c49f
Thomas Liu <tliu020@ucr.edu>
2016-03-13 20:32:33 -0700

    added create_db.sh

--- sql/scripts/create_db.sh
+++ sql/scripts/create_db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-psql -p 1073 $DB_NAME < $DIR/../src/create_tables.sql
-psql -p 1073 $DB_NAME < $DIR/../src/create_indexes.sql
-psql -p 1073 $DB_NAME < $DIR/../src/load_data.sql
+psql -p $PGPORT $DB_NAME < $DIR/../src/create_tables.sql
+psql -p $PGPORT $DB_NAME < $DIR/../src/create_indexes.sql
+psql -p $PGPORT $DB_NAME < $DIR/../src/load_data.sql
+psql -p $PGPORT $DB_NAME < $DIR/../src/triggers.sql
